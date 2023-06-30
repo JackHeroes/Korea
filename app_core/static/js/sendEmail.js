22062b834c9bb480cf9304b152609fc80ff10c8d
João Felipe Correia de Oliveira <101203356+JackHeroes@users.noreply.github.com>
2023-06-30 16:31:47 -0300

    enviar emails

---
+++ app_core/static/js/sendEmail.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("form-contact");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var formData = new FormData(form);
+        fetch(form.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken')
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            if (data.status === 'success') {
+                showAlert('success', 'Formulário enviado com sucesso!');
+                form.reset();
+            } else if (data.status === 'error') {
+                showAlert('error', 'Erro ao enviar formulário: ' + data.message);
+            }
+        })
+        .catch(function(error) {
+            showAlert('error', 'Erro ao enviar formulário.');
+        });
+    });
+
+    function showAlert(type, message) {
+        var alertContainer = document.getElementById('alert-container');
+        var alertClass = type === 'success' ? 'alert-success' : 'alert-danger';
+        var alertHTML = '<div class="alert ' + alertClass + '">' + message + '</div>';
+        alertContainer.innerHTML = alertHTML;
+        setTimeout(function() {
+            alertContainer.innerHTML = '';
+        }, 5000); // Remove o alerta após 5 segundos
+    }
+
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+});
--- app_core/views.py
+++ app_core/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+    
+def enviar_email(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        mensagem = request.POST['mensagem']
+
+        # Configuração do e-mail
+        remetente = settings.EMAIL_HOST_USER
+        destinatario = 'JohnHeroes@outlook.com.br'
+        assunto = 'Novo formulário de contato recebido'
+        
+        # Corpo do e-mail
+        texto = f'''
+        Nome: {nome}
+        E-mail: {email}
+        Mensagem: {mensagem}
+        '''
+
+        try:
+            # Configuração e envio do e-mail
+            send_mail(
+                subject=assunto,
+                message=texto,
+                from_email=remetente,
+                recipient_list=[destinatario],
+                fail_silently=False,
+            )
+
+            # Retorna uma resposta JSON indicando sucesso
+            return JsonResponse({'status': 'success'})
+        except Exception as e:
+            # Retorna uma resposta JSON indicando o erro ocorrido
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    # Se o método da requisição não for POST, retorne a página normalmente
+    return render(request, 'index.html')
