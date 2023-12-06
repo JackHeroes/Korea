e00cf9cbcc272da4cf94e2dccd6373b2ebba08e8
João Felipe Correia de Oliveira <101203356+JackHeroes@users.noreply.github.com>
2023-12-05 22:55:04 -0300

    Delete app_core directory

--- app_core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app_core import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('send-email/', views.form_sendEmail, name='send-email'),
-]
