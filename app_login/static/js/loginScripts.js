dae14be6de24fcdecb2f42573c462ca63bde14f4
João Felipe Correia de Oliveira <101203356+JackHeroes@users.noreply.github.com>
2023-12-05 22:55:10 -0300

    Delete app_login directory

--- app_login/static/js/loginScripts.js
+++
@@ -1,9 +0,0 @@
-function showPassword(inputId) {
-    const input = document.getElementById(inputId);
-    
-    if (input && input.type === 'password') {
-      input.type = 'text';
-    } else if (input) {
-      input.type = 'password';
-    }
-}
